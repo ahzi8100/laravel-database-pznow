9f3b10aa99ddcee075cb5b44c2a42a6638313a15
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-16 17:32:20 +0700

    Delete QueryBuilder

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -150,4 +150,13 @@ class QueryBuilderTest extends TestCase
             Log::info(json_encode($item));
         });
     }
+
+    public function testDelete()
+    {
+        $this->insertCategories();
+
+        DB::table('categories')->where('id','=','SMARTPHONE')->delete();
+        $collection = DB::table('categories')->where('id', '=','SMARTPHONE')->get();
+        assertCount(0, $collection);
+    }
 }
