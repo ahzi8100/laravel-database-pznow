5f786db1df20b187e13758355f49d07c6d405e28
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-16 13:47:28 +0700

    Make Table

---
+++ database.sql
@@ -0,0 +1,9 @@
+create table categories
+(
+    id      varchar(100) not null primary key,
+    name    varchar(100) not null ,
+    description text,
+    created_at  timestamp
+) engine innodb;
+
+desc categories;
