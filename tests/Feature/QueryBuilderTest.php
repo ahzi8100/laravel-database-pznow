d0edf314823dbb118dfa59ac60cb039b41eddf1c
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-16 16:42:49 +0700

    Where QueryBuilder

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use PhpParser\Builder;
 use Tests\TestCase;
 use function PHPUnit\Framework\assertEquals;
 
@@ -38,6 +39,33 @@ class QueryBuilderTest extends TestCase
         $collection = DB::table('categories')->select(['id','name'])->get();
         self::assertNotNull($collection);
 
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function insertCategories()
+    {
+        DB::table('categories')
+            ->insert(['id'=>'SMARTPHONE', 'name'=>'Smartphone','created_at'=>'2020-01-01 00:00:00']);
+        DB::table('categories')
+            ->insert(['id'=>'FOOD', 'name'=>'Food','created_at'=>'2020-01-01 00:00:00']);
+        DB::table('categories')
+            ->insert(['id'=>'LAPTOP', 'name'=>'Laptop','created_at'=>'2020-01-01 00:00:00']);
+        DB::table('categories')
+            ->insert(['id'=>'FASHION', 'name'=>'Fashion','created_at'=>'2020-01-01 00:00:00']);
+    }
+
+    public function testWhere()
+    {
+        $this->insertCategories();
+
+        $collection = DB::table('categories')->Where(function (\Illuminate\Database\Query\Builder $builder) {
+            $builder->where('id', '=', 'SMARTPHONE');
+            $builder->orWhere('id', '=', 'LAPTOP');
+        })->get();
+
+        self::assertCount(2, $collection);
         $collection->each(function ($item) {
             Log::info(json_encode($item));
         });
